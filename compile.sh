2898c26dc6f68709d99400da3abc3b44206bc5aa
peiyu <pc00083@gmail.com>
2017-04-24 22:23:32 +0800

    revise the method of compile

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-javac -cp graph/dist/JavaPlot.jar KMeans.java Cluster.java Point.java
-java -cp graph/dist/JavaPlot.jar:. KMeans input2.data cluster.data
+javac -cp graph/dist/JavaPlot.jar KMeans_nofile.java Cluster.java Point.java
+java -cp graph/dist/JavaPlot.jar:. KMeans_nofile input2.data cluster.data
--- compile_tri.sh
+++ compile_tri.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-javac -cp graph/dist/JavaPlot.jar KMeans_tri.java Cluster.java Point.java
-java -cp graph/dist/JavaPlot.jar:. KMeans_tri input.data cluster.data
+inputfile=$1
+cluster=$2
+num_cluster=$3
+
+javac -cp graph/dist/JavaPlot.jar KMeans_tri_nofile.java Cluster.java Point.java
+java -cp graph/dist/JavaPlot.jar:. KMeans_tri_nofile $inputfile $cluster $num_cluster
